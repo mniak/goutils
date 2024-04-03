065c6883cbed1d8a33fb36163fa3900071bb8165
Andre Soares <andregsilv@gmail.com>
2024-04-03 12:06:46 -0300

    verbatims

--- smartraw_test.go
+++ smartraw_test.go
@@ -6,16 +6,51 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSmartRaw(t *testing.T) {
+func TestSmartRaw_SimpleScenarios_ShouldReturnVerbatim(t *testing.T) {
 	testCases := []struct {
 		name   string
 		input  string
 		output string
 	}{
 		{
-			name:   "Simple string",
+			name:   "Single line",
 			input:  "hello world",
 			output: "hello world",
+		},
+		{
+			name:   "Two lines LF",
+			input:  "hello\nworld",
+			output: "hello\nworld",
+		},
+		{
+			name:   "Two lines CRLF",
+			input:  "hello\r\nworld",
+			output: "hello\r\nworld",
+		},
+		{
+			name:   "Two lines LFCR",
+			input:  "hello\n\rworld",
+			output: "hello\n\rworld",
+		},
+		{
+			name:   "Two lines, second is idented with four spaces",
+			input:  "hello\n  world",
+			output: "hello\n  world",
+		},
+		{
+			name:   "Two lines, second is idented with four spaces",
+			input:  "hello\n    world",
+			output: "hello\n    world",
+		},
+		{
+			name:   "Two lines, second is idented with one tab",
+			input:  "hello\n\tworld",
+			output: "hello\n\tworld",
+		},
+		{
+			name:   "Two lines, second is idented with two tabs",
+			input:  "hello\n\t\tworld",
+			output: "hello\n\t\tworld",
 		},
 	}
 	for _, tc := range testCases {
