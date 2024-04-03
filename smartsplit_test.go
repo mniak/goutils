c6f64fabdc53879a81c93520335c2aee0c5a1982
Andre Soares <andregsilv@gmail.com>
2024-04-03 12:22:11 -0300

    smartsplit

---
+++ smartsplit_test.go
@@ -0,0 +1,57 @@
+package goutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSmartSplitLines(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		expected []string
+	}{
+		{
+			name:     "Empty",
+			input:    "",
+			expected: []string{""},
+		},
+		{
+			name:     "Single line",
+			input:    "single line",
+			expected: []string{"single line"},
+		},
+		{
+			name:     "Three lines LF",
+			input:    "one\ntwo\nthree",
+			expected: []string{"one", "two", "three"},
+		},
+		{
+			name:     "With new line at the end",
+			input:    "one\ntwo\nthree\n",
+			expected: []string{"one", "two", "three", ""},
+		},
+		{
+			name:     "Three lines CRLF",
+			input:    "one\r\ntwo\r\nthree",
+			expected: []string{"one", "two", "three"},
+		},
+		{
+			name:     "Three lines LFCR",
+			input:    "one\n\rtwo\n\rthree",
+			expected: []string{"one", "two", "three"},
+		},
+		{
+			name:     "Four lines mixed LF CRLF LFCR",
+			input:    "one\ntwo\r\nthree\n\rfour",
+			expected: []string{"one", "two", "three", "four"},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			output := SmartSplitLines(tc.input)
+			assert.Equal(t, tc.expected, output)
+		})
+	}
+}
